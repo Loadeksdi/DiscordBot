6f0f60c928b418d0c3ab55ec9dad0fe829e6a884
Loadeksdi <nicolas.load@gmail.com>
2021-01-19 13:26:08 +0100

    Fix: Heartbeat disconnect

--- twitter.js
+++ twitter.js
@@ -4,11 +4,11 @@ const { URL } = require('url');
 const fetch = require('node-fetch');
 
 class TwitterEvents extends EventEmitter {
-  connectionOpened = false
-
+  connectionOpened = false;
+  heartbeatTimeout = null;
   constructor() {
     super();
-    this.startStream()
+    this.startStream();
   }
   startStream() {
     if (this.connectionOpened === true) return
@@ -27,23 +27,24 @@ class TwitterEvents extends EventEmitter {
       cache: 'default',
       timeout: 0
     };
-    const restart = () => {
-      this.connectionOpened = false;
-      this.startStream();
-    };
+
     fetch(url, initGet).then(response => {
       console.log("Succesfully connected");
       this.connectionOpened = true;
       response.body.on('data', (data) => this.readData(data));
-      response.body.on('error', () => setTimeout(restart, 30 * 1000));
-      response.body.on('end', restart);
-      response.body.on('close', restart);
+      response.body.on('error', () => setTimeout(this.reconnect, 30 * 1000));
+      response.body.on('end', this.reconnect);
+      response.body.on('close', this.reconnect);
     }).catch(err => {
       console.error(err);
       setTimeout(restart, 30 * 1000);
     });
   }
   readData(chunk) {
+    if (this.heartbeatTimeout !== null) {
+      clearTimeout(this.heartbeatTimeout);
+    }
+    this.heartbeatTimeout = setTimeout(this.reconnect, 21 * 1000);
     if (chunk.length <= 2) {
       return;
     }
@@ -59,6 +60,10 @@ class TwitterEvents extends EventEmitter {
     else {
       this.emit('tweet', data, `https://twitter.com/0/status/${data.data.id}`);
     }
+  }
+  reconnect() {
+    this.connectionOpened = false;
+    this.startStream();
   }
 };
 
