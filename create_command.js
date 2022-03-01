3ddf3269f66f05883eaf50026a4580c46e32df98
Loadeksdi <nicolas.load@gmail.com>
2022-03-01 01:13:29 +0100

    Remove cached slash command

--- create_command.js
+++ create_command.js
@@ -21,11 +21,11 @@ const commands = [
 		)
 ].map(command => command.toJSON())
 
-rest.get(Routes.applicationGuildCommands(clientId, guildId))
+rest.get(Routes.applicationCommands(clientId))
 	.then(data => {
 		const promises = []
 		for (const command of data) {
-			const deleteUrl = `${Routes.applicationGuildCommands(clientId, guildId)}/${command.id}`
+			const deleteUrl = `${Routes.applicationCommands(clientId)}/${command.id}`
 			promises.push(rest.delete(deleteUrl))
 		}
 		return Promise.all(promises)
